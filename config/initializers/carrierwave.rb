c33b09ba6c5f9e2a2b1c7ea5085f677bb2d46ac4
Nic0b <nicob69@ymail.com>
2020-03-17 15:27:17 +0100

    fix s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,8 +20,9 @@ if Rails.application.secrets.aws_access_key_id.present?
       provider:              'AWS',                                             # required
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+      # use_iam_profile:       true,
       region:                'eu-west-3',                                       # optional, defaults to 'us-east-1'
-      host:                  's3.eu-west-3.amazonaws.com',                                  # optional, defaults to nil
+      # host:                  's3.eu-west-3.amazonaws.com',                                  # optional, defaults to nil
     }
     config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'openairthp')              # required
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}", 'X-Content-Type-Options' => "nosniff" }    # optional, defaults to {}
