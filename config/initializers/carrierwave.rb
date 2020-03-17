cab0c905e9378641b9508f1d2dfdf816df643faf
Nic0b <nicob69@ymail.com>
2020-03-17 14:19:34 +0100

    fix s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,28 @@
 
 # Default CarrierWave setup.
 #
+
+CarrierWave.configure do |config|
+  config.permissions = 0o666
+  config.directory_permissions = 0o777
+  config.storage = :file
+  config.enable_processing = !Rails.env.test?
+end
+
+if Rails.application.secrets.aws_access_key_id.present?
   require "carrierwave/storage/fog"
 
   CarrierWave.configure do |config|
     config.storage = :fog
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog/aws'                                             # required
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY_ID"),
-      aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
-      region:                'eu-west-3',
+      provider:              'AWS',                                             # required
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+      region:                'eu-west-3',                                       # optional, defaults to 'us-east-1'
+      host:                  's3.eu-west-3.amazonaws.com',                                  # optional, defaults to nil
     }
-    config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'openairthp')
-    config.fog_public     = false
-    config.fog_attributes = {
-      'Cache-Control' => "max-age=#{365.day.to_i}",
-      'X-Content-Type-Options' => "nosniff"
-    }
+    config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'openairthp')              # required
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
   end
+end
